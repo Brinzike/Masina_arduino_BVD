10b3a48ae11a219397024948a3e3fae05fe962ac
Brinza Victor-Daniel <brinzike@gmail.com>
2020-04-29 17:53:48 +0300

    Initializare ceas

---
+++ Cod_Slave/ceas_BVD.c
@@ -0,0 +1,19 @@
+#include "Arduino.h"
+
+void ceas(byte zeci_secunde, unsigned int secunde_curente, boolean tic_tac_secunde)
+{
+    zeci_secunde = ((millis() % 1000) / 100) + 1;
+    
+    if( zeci_secunde == 10)
+    {
+        if(tic_tac_secunde == 1)
+        {
+            secunde_curente++;
+            tic_tac_secunde = 0;
+        }
+    }
+    else
+    {
+        tic_tac_secunde = 1;
+    }
+}
