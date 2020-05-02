ecc6bb6cf8b76c6545b7f0200ff8dca3f6934a30
Brinza Victor-Daniel <brinzike@gmail.com>
2020-05-02 18:55:18 +0300

    Coduri Slave-Master

---
+++ Cod_Slave/coduri_slave_master_BVD.c
@@ -0,0 +1,29 @@
+// Coduri de la Slave -> Master
+// 101 - Nu ai butoanele telecomenzii configurate.
+// 102 - Incepem sa configuram butoanele.
+// 103 - Apasa o tasta pentru a seta butonul `Bine`.
+// 104 - Ai setat butonul 'Bine'.
+// 105 - Apasa o tasta pentru seta butonul `Anulare`.
+// 106 - Ai setat butonul 'Anulare'.
+// 107 - Apasa o tasta pentru seta butonul `Inainte`.
+// 108 - Ai setat butonul 'Inainte'.
+// 109 - Apasa o tasta pentru seta butonul `Inapoi`.
+// 110 - Ai setat butonul 'Inapoi'.
+// 111 - Apasa o tasta pentru seta butonul `Stanga`.
+// 112 - Ai setat butonul 'Stanga'.
+// 113 - Apasa o tasta pentru seta butonul `Dreapta`.
+// 114 - Ai setat butonul 'Dreapta'.
+
+
+// 120 - Ai butoane configurate.
+// -=- - Daca doresti sa configurezi alta telecomanda.
+// -=- - Apasa orice tasta in afata de 'Bine".
+// -=- - Daca doresti sa continui cu butoanele configurate, apasa 'Bine'.
+// 121 - Ai pastrat configurarea butoanelor.
+
+
+// 150 - Acesta tasta este deja utilizata.
+// -=- - Te rog sa incerci din nou o alta tasta
+// 151 - Am primit un semnal.
+// -=- - Apasa pe acelasi buton pentru a confirma.
+// 152 - Nu a fost acelasi buton. 
